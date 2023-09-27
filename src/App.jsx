f621c4d07dff8595a2a6f43dfd9a3ead1abb28b2
Md   Razib Sarker <mdrajibsorkar965@gmail.com>
2023-09-27 17:27:40 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+import Blogs from './Components/Blogs-components/Blogs'
+
+function App() {
+  
+
+  return (
+    <>  
+      <h1 className='text-center text-3xl mt-8'>Vite + React</h1>
+      <Blogs></Blogs>
+      
+    </>
+  )
+}
+
+export default App
